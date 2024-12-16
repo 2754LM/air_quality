b35aa992e2d393f9521bda340fde6f74f5ac7fff
kano <2754305745@qq.com>
2024-12-16 20:37:16 +0800

    前端代码

--- backend/app.py
+++ backend/app.py
@@ -2,26 +2,28 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse, HTMLResponse
 import os
 import json  # 导入 json 模块
-
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
+app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../frontend/static')), name="static")
 
 @app.get("/")
-async def read_root():
+def read_root():
     return {"message": "Welcome to the FastAPI server!"}
 
 @app.get("/test")
-async def get_test_data():
+def get_test_data():
     file_path =  os.path.join(os.path.dirname(__file__), 'data/processed/test.json')
     with open(file_path, 'r', encoding='utf-8') as f:
         content = json.load(f) 
     return JSONResponse(content=content)
 
-@app.get("/chart")
-async def get_chart():
-    file_path = os.path.join(os.path.dirname(__file__), '../frontend/test.html')
+@app.get("/index")
+def get_chart():
+    file_path = os.path.join(os.path.dirname(__file__), '../frontend/index.html')
     with open(file_path, 'r', encoding='utf-8') as f:
         return HTMLResponse(content=f.read())
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
