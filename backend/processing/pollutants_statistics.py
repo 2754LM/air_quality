e57ee7412e7a239a585c489b3b05e991e04a7b47
st0rmed <st0rm43732@gmail.com>
2024-12-18 10:15:36 +0800

    Update and rename pollutants_statics.py to pollutants_statistics.py

---
+++ backend/processing/pollutants_statistics.py
@@ -0,0 +1,24 @@
+import json
+import os
+from collections import Counter
+
+def pollutant_statistics():
+    
+    with open(os.path.join(os.path.dirname(__file__) ,'../data/processed/pollutants_all.json'), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    pollutants = [value for value in data.values() if value != "NA"]
+    pollutant_counts = Counter(pollutants)
+
+
+    result = {
+        "title": "全国主要污染物统计",
+        "categories": list(pollutant_counts.keys()),
+        "value": list(pollutant_counts.values()),
+        "seriesname": "省份数量"
+    }
+
+    with open(os.path.join(os.path.dirname(__file__),'../data/processed/pollutant_statistics.json'), 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=4)
+    
+    
