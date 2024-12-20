fec4d4dde0742888a0ef278e35d7a8b7c6a9efe8
kano <2754305745@qq.com>
2024-12-20 11:29:21 +0800

    用户数据库

---
+++ backend/mysql.py
@@ -0,0 +1,12 @@
+import pymysql
+
+def check_user(username, password):
+    connection = pymysql.connect(
+        host='47.238.226.55',
+        user='root',
+        password='Aa_123456',
+        database='air_database'
+    )
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s;", (username, password))
+        return cursor.fetchone() is not None 
